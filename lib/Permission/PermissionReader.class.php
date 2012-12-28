3f4ae010e90722e5b4c09b2fefa61e9ae2f40472
Klauenbösch Christian <christian@klit.ch>
2012-12-28 16:06:53 +0100

    fixed #6 commit before

---
+++ lib/Permission/PermissionReader.class.php
@@ -0,0 +1,26 @@
+<?php
+
+    /**
+     * [PermissionReader.class.php]
+     * @version 1.0.0
+     * @author Christian Klauenbösch
+     * @copyright Klauenbösch IT Services
+     * @link http://www.klit.ch
+     *
+     * previous     now     what changed
+     *              1.0.0   -
+     *
+     */
+    class PermissionReader {
+
+        public function __construct() {
+
+        }
+
+        public function __desctruct() {
+
+        }
+
+    }
+
+?>
